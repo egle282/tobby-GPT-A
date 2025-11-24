e2dd279095001d17937e7e6a17fdb04ec79621da
egle282 <kovalev282@yahoo.com>
2025-11-24 16:50:34 +0300

    Update config.py

--- supportbot/config.py
+++ supportbot/config.py
@@ -1,1 +1,9 @@
+python
+import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN") or "ВАШ_ТОКЕН"
+ADMIN_IDS = [int(uid) for uid in os.getenv("ADMIN_IDS", "ваш_ID_админа").split(",")]
+
+DATA_PATH = 'data/'
+FAQ_FILE = DATA_PATH + 'faq.json'
+HISTORY_FILE = DATA_PATH + 'user_history.json'
