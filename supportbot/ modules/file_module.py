96f752b5fb040907d154b4c6be89c0bbd78c5038
egle282 <kovalev282@yahoo.com>
2025-11-24 16:52:49 +0300

    Update file_module.py

--- supportbot/ modules/file_module.py
+++ supportbot/ modules/file_module.py
@@ -1,1 +1,13 @@
+python
+class FileModule:
+    def __init__(self, bot, feature_on_fn):
+        self.bot = bot
+        self.feature_on = feature_on_fn
 
+    def handle(self, msg):
+        if not self.feature_on('file_module'): return False
+        if msg.content_type not in ['document','photo']:
+            return False
+        self.bot.send_message(msg.chat.id, "Файл получен. Оператор сможет его посмотреть.")
+        # Здесь логика сохранения/пересылки
+        return True
