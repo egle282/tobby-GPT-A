b5d6d3608fa320dc4d4a65bea1afe22e7d6bc62b
egle282 <kovalev282@yahoo.com>
2025-11-24 16:59:25 +0300

    Update file_module.py

--- supportbot/ modules/file_module.py
+++ supportbot/ modules/file_module.py
@@ -5,9 +5,15 @@ class FileModule:
         self.feature_on = feature_on_fn
 
     def handle(self, msg):
-        if not self.feature_on('file_module'): return False
-        if msg.content_type not in ['document','photo']:
+        if not self.feature_on('file_module'):
             return False
-        self.bot.send_message(msg.chat.id, "Файл получен. Оператор сможет его посмотреть.")
-        # Здесь логика сохранения/пересылки
-        return True
+        if msg.content_type == 'document':
+            file_info = self.bot.get_file(msg.document.file_id)
+            file_path = file_info.file_path
+            # For demo: just inform user
+            self.bot.send_message(msg.chat.id, "Файл получен и принят.")
+            return True
+        elif msg.content_type == 'photo':
+            self.bot.send_message(msg.chat.id, "Фото получено и принято.")
+            return True
+        return False
