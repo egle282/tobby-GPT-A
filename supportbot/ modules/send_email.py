aeab24db09f1fb43bb6a65cabe183a5c93443cb6
egle282 <kovalev282@yahoo.com>
2025-11-25 13:18:59 +0300

    Update send_email.py

--- supportbot/ modules/send_email.py
+++ supportbot/ modules/send_email.py
@@ -2,27 +2,34 @@ python
 """
 send_email.py
 -------------
-Позволяет переслать сообщения пользователя на e-mail службы поддержки командой /email.
+Позволяет пользователю отправить e-mail в поддержку через команду /email.
 """
-import os, yagmail
-
-SUPPORT_EMAIL = os.getenv("SUPPORT_EMAIL")
-SUPPORT_SMTP_USER = os.getenv("SMTP_USER")
-SUPPORT_SMTP_PASS = os.getenv("SMTP_PASS")
+import yagmail
+from config import *
 
 class SendEmail:
     def __init__(self, bot, feature_on_fn):
         self.bot = bot
         self.feature_on = feature_on_fn
-        self.client = yagmail.SMTP(user=SUPPORT_SMTP_USER, password=SUPPORT_SMTP_PASS)
+        try:
+            self.client = yagmail.SMTP(user=SMTP_USER, password=SMTP_PASS)
+        except Exception as e:
+            print("Не удалось подключиться к SMTP:", e)
 
     def handle(self, msg):
         if not self.feature_on('send_email'):
             return False
-        if msg.text and msg.text.startswith("/email "):
-            theme = "Запрос в поддержку " + str(msg.from_user.id)
-            content = msg.text[len("/email "):]
-            self.client.send(to=SUPPORT_EMAIL, subject=theme, contents=content)
-            self.bot.send_message(msg.chat.id, "Ваше сообщение отправлено в поддержку на email!")
+        if msg.text and msg.text.lower().startswith("/email "):
+            if not SUPPORT_EMAIL or not SMTP_USER or not SMTP_PASS:
+                self.bot.send_message(msg.chat.id, "Отправка email временно недоступна.")
+                return True
+            subject = f"Бот Helpino, запрос от {msg.from_user.id}"
+            text = msg.text[len("/email "):]
+            try:
+                self.client.send(to=SUPPORT_EMAIL, subject=subject, contents=text)
+                self.bot.send_message(msg.chat.id, "Ваше сообщение отправлено в службу поддержки по email.")
+            except Exception as e:
+                self.bot.send_message(msg.chat.id, "Ошибка отправки email.")
+                print("SMTP error:", e)
             return True
         return False
