b191a9bccc92991bac963109d1090756765f9a40
egle282 <kovalev282@yahoo.com>
2025-11-25 12:38:11 +0300

    Update push_notify.py

--- supportbot/ modules/push_notify.py
+++ supportbot/ modules/push_notify.py
@@ -1,20 +1,33 @@
-```python
+python
+"""
+push_notify.py
+--------------
+Модуль push-уведомлений. Позволяет пользователям подписываться на обновления.
+Push-уведомления рассылаются через вызов метода send_push(text) по всем подписчикам.
+"""
+
 class PushNotify:
     def __init__(self, bot, feature_on_fn):
+        """
+        :param bot: объект telebot.TeleBot
+        :param feature_on_fn: функция проверки статуса модуля
+        """
         self.bot = bot
         self.feature_on = feature_on_fn
         self.users = set()
 
     def handle(self, msg):
+        """Позволяет подписаться на push через запрос содержаший 'подпис'. """
         if not self.feature_on('push_notify'):
             return False
         if msg.text and 'подпис' in msg.text.lower():
             self.users.add(msg.chat.id)
-            self.bot.send_message(msg.chat.id, "Теперь вы будете получать push-уведомления.")
+            self.bot.send_message(msg.chat.id, "Теперь вы будете получать push-уведомления!")
             return True
         return False
-# Для отправки уведомлений  
+
     def send_push(self, text):
+        """Рассылает заданный текст по всем подписчикам."""
         for uid in self.users:
             try:
                 self.bot.send_message(uid, f"🔔 {text}")
