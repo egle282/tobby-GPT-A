07c17b8bc3b147c4643d70ff213f52122a0613a9
egle282 <kovalev282@yahoo.com>
2025-11-26 21:22:20 +0300

    Update voice_module.py

--- modules/voice_module.py
+++ modules/voice_module.py
@@ -1,24 +1,27 @@
 """
 Распознает голосовые сообщения с помощью SpeechRecognition, пересылает текст оператору/боту.
 """
-
 class VoiceModule:
-    def __init__(self, bot, feature_on_fn):
+    def __init__(self, bot, is_enabled_cb):
         self.bot = bot
-        self.feature_on = feature_on_fn
-        self.expecting_voice = set()
+        self.is_enabled = is_enabled_cb
+        self.await_voice = set()
         def handle(self, msg):
-        if not self.feature_on('voice_module'):
+        if not self.is_enabled('voice_module'):
             return False
+
         if msg.text == "🎤 Голосовое":
-            self.expecting_voice.add(msg.from_user.id)
-            self.bot.send_message(msg.chat.id, "Пожалуйста, отправьте голосовое сообщение для обработки!")
+            self.await_voice.add(msg.from_user.id)
+            self.bot.send_message(msg.chat.id, "Жду голосовое сообщение!")
             return True
-        if msg.from_user.id in self.expecting_voice and msg.content_type == "voice":
-            self.expecting_voice.remove(msg.from_user.id)
-            self.bot.send_message(msg.chat.id, "Голосовое получено! (Расшифровка включится позже.)")
-            return True
-        if msg.from_user.id in self.expecting_voice:
-            self.bot.send_message(msg.chat.id, "Жду именно голосовое сообщение.")
-            return True
+
+        if msg.from_user.id in self.await_voice:
+            if msg.content_type == "voice":
+                self.await_voice.remove(msg.from_user.id)
+                self.bot.send_message(msg.chat.id, "Голосовое получено. (Здесь можно добавить распознавание текста)")
+                return True
+            else:
+                self.bot.send_message(msg.chat.id, "Жду именно голосовое сообщение.")
+                return True
+
         return False
