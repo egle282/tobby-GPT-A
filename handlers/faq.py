1090306ee491c591de90403d945c18feb0b6e8cf
egle282 <kovalev282@yahoo.com>
2025-11-27 19:12:22 +0300

    Update faq.py

--- handlers/faq.py
+++ handlers/faq.py
@@ -1,1 +1,7 @@
+from bot import bot
+from utils.keyboards import kb_main
 
+@bot.message_handler(func=lambda msg: msg.text == "FAQ")
+def handle_faq(msg):
+    bot.send_message(msg.chat.id, "Введи вопрос или выбери из популярных.", reply_markup=kb_main())
+    # Тут твоя логика
