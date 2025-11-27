864a992b216227089ac4adf5652f6d2d27facada
egle282 <kovalev282@yahoo.com>
2025-11-27 19:20:25 +0300

    Update menu.py

--- handlers/menu.py
+++ handlers/menu.py
@@ -9,6 +9,11 @@ def handle_start(msg):
         reply_markup=kb_main()
     )
 
+@bot.message_handler(commands=['donate'])
+def handle_donate(msg):
+    bot.send_message(msg.chat.id, "Спасибо за интерес к поддержке!")
+    bot.send_message(msg.chat.id, "Главное меню:", reply_markup=kb_main())
+
 @bot.message_handler(func=lambda msg: msg.text == "Главное меню")
 def handle_main_menu(msg):
     bot.send_message(msg.chat.id, "Главное меню:", reply_markup=kb_main())
