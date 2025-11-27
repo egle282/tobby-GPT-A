4a72719aa8721926a090729e96992f3c83d7b644
egle282 <kovalev282@yahoo.com>
2025-11-27 19:12:58 +0300

    Update news.py

--- handlers/news.py
+++ handlers/news.py
@@ -1,1 +1,6 @@
-
+from bot import bot
+from utils.states import set_state, get_state, clear_state
+from utils.keyboards import kb_main
+@bot.message_handler(func=lambda msg: msg.text == "📢 Новости")
+def news_start(msg):
+    bot.send_message(msg.chat.id, "Функция новостей!") # Вставь свою логику
