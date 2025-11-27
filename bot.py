074e4f4bb4c247528484d6ac4094fd4cdd4ae88c
egle282 <kovalev282@yahoo.com>
2025-11-27 21:02:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 from config import BOT_TOKEN
 import telebot
 
+# Создай экземпляр бота
 bot = telebot.TeleBot(BOT_TOKEN, threaded=False)
 
-# Инициализация всех хендлеров (ВАЖНО: импортировать после создания bot!)
+# Важно: все хендлеры импортируются после создания bot!
 import handlers.menu
 import handlers.qr
 import handlers.news
@@ -11,6 +12,10 @@ import handlers.faq
 import handlers.voice
 import handlers.email
 import handlers.donate
+
+# Подключаем новые обработчики:
+import handlers.common_features   # здесь /start, /help, бесплатные команды
+import handlers.premium_features # здесь премиальные/лимитные команды
 
 if __name__ == '__main__':
     bot.infinity_polling()
