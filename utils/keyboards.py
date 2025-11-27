05c6c089cc8c9669958bfc7a7dc102adfa8df671
egle282 <kovalev282@yahoo.com>
2025-11-27 19:10:20 +0300

    Update keyboards.py

--- utils/keyboards.py
+++ utils/keyboards.py
@@ -1,1 +1,14 @@
+from telebot.types import ReplyKeyboardMarkup
 
+def kb_main():
+    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+    kb.add("📎 Файл", "📷 QR-сканер")
+    kb.add("📢 Новости", "🎤 Голосовое")
+    kb.add("FAQ", "Отправить Email")
+    kb.add("Главное меню")
+    return kb
+
+def kb_qr():
+    kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    kb.add("Сделать фото сразу", "Главное меню")
+    return kb
