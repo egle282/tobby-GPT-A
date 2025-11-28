ad3916e962f85a202e9598cc1ec5bf5524013976
egle282 <kovalev282@yahoo.com>
2025-11-28 11:03:42 +0300

    Update callbacks.py

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -1,13 +1,24 @@
 # utils/callbacks.py
 
 def always_true(*args, **kwargs):
-    """Заглушка: всегда возвращает True"""
+    """Всегда возвращает True."""
     return True
 
 def always_false(*args, **kwargs):
-    """Заглушка: всегда возвращает False"""
+    """Всегда возвращает False."""
     return False
 
-def echo_args(*args, **kwargs):
-    """Пример: просто возвращает переданные аргументы"""
-    return args, kwargs
+def is_subscriber(user_id, *args, **kwargs):
+    """
+    Проверка подписки: пример заглушки, всегда возвращает True.
+    На практике реализуй тут проверку по user_id.
+    """
+    return True
+
+def is_admin(user_id, *args, **kwargs):
+    """
+    Заглушка на проверку роли администратора.
+    Сравнивай user_id с заданными ID админов.
+    """
+    ADMIN_IDS = [12345678]  # замени на реальные ID
+    return user_id in ADMIN_IDS
